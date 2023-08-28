440e810a9684f9ae53fb77f0dbe64bfa5ff9b505
Manoj Mantha <manojputtaluckey@gmail.com>
2023-08-28 16:27:29 +0000

    Node

--- greeting/index.js
+++ greeting/index.js
@@ -2,5 +2,5 @@ module.exports = "Have a Great Day";
 const word = require("../message/index");
 
 const greeting = require("../message/index");
-const result = `Hello Rahul! ${greeting}`;
+const result = `${greeting}`;
 console.log(result);
